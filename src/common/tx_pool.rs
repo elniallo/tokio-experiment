d3fe09a6db51ca9176594d1533fc14d9a2b1b3f8
Niall Moore <niall@Nialls-iMac.local>
2018-09-07 11:00:04 +0900

    Get transactions from an address as a Vector of Signed Txs

--- src/common/tx_pool.rs
+++ src/common/tx_pool.rs
@@ -1,13 +1,13 @@
-use common::address::Address;
+use common::address::{Address, ValidAddress};
 use common::signed_tx::SignedTx;
 
 use std::cmp::Ordering;
 use std::cmp::PartialOrd;
 use std::collections::BinaryHeap;
-
+#[derive(Debug, Clone)]
 pub struct ITxQueue {
     pub sum: u64,
-    pub queue: Vec<SignedTx>,
+    pub queue: BinaryHeap<SignedTx>,
     pub address: String,
 }
 
@@ -62,7 +62,13 @@ impl TxPool {
     }
 
     pub fn get_txs_of_address(&self, address: &Address) -> Vec<SignedTx> {
-        Vec::new()
+        let add = address.to_string();
+        for account in &self.pool {
+            match &account.address {
+                add => return account.queue.clone().into_sorted_vec(),
+            };
+        }
+        vec![]
     }
 
     pub fn prepare_for_broadcast(&self) -> Vec<SignedTx> {
@@ -149,4 +155,45 @@ mod tests {
     }
     #[test]
     fn should_get_txs() {}
+
+    #[test]
+    fn should_get_txs_from_address() {
+        // Initialise pool
+        let mut tx_pool = TxPool::new();
+        //Check Empty for missing Address
+        let to_addr = "H4JSXdLtkXVs6G7fk2xea1dB4hTgQ3ps6".to_string();
+        let to = Address::from_string(&to_addr).unwrap();
+        assert_eq!(tx_pool.get_txs_of_address(&to).len(), 0);
+        let from_addr = "H27McLosW8psFMbQ8VPQwXxnUY8QAHBHr".to_string();
+        let from = Address::from_string(&from_addr).unwrap();
+        let amount = 100;
+        let fee = 1;
+        let nonce = 1;
+        let recovery = RecoveryId::from_i32(0).unwrap();
+
+        let signature_bytes = [
+            208, 50, 197, 4, 84, 254, 196, 173, 123, 37, 234, 93, 48, 249, 247, 56, 156, 54, 7,
+            211, 17, 121, 174, 74, 111, 1, 7, 184, 82, 196, 94, 176, 73, 221, 78, 105, 137, 12,
+            165, 212, 15, 47, 134, 101, 221, 69, 158, 19, 237, 120, 63, 173, 92, 215, 144, 224,
+            100, 78, 84, 128, 237, 25, 234, 206,
+        ];
+        let secp = Secp256k1::without_caps();
+        let signature =
+            RecoverableSignature::from_compact(&secp, &signature_bytes, recovery).unwrap();
+
+        let signed_tx = SignedTx::new(from, to, amount, fee, nonce, signature, recovery);
+        let mut queue = ITxQueue {
+            sum: fee,
+            queue: BinaryHeap::new(),
+            address: from_addr,
+        };
+        queue.queue.push(signed_tx);
+        tx_pool.pool.push(queue.clone());
+        let q = tx_pool.pool.peek();
+        match q {
+            Some(q) => assert_eq!(q, &queue),
+            None => {}
+        };
+        assert_eq!(tx_pool.get_txs_of_address(&from).len(), 1);
+    }
 }
