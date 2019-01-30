deeb5dc41d56be21c520a29e357069d6d54948d3
elniallo <niall@hycon.io>
2019-01-30 16:21:45 +0900

    Updated build script

--- build.rs
+++ build.rs
@@ -5,7 +5,12 @@ fn main() {
     protoc_rust_no_elision::run(protoc_rust_no_elision::Args {
         out_dir: "src/serialization",
         input: &[
-            "src/proto/protocol.proto",
+            "src/proto/block.proto",
+            "src/proto/blockHeader.proto",
+            "src/proto/network.proto",
+            "src/proto/peer.proto",
+            "src/proto/state.proto",
+            "src/proto/tx.proto",
         ],
         includes: &["src/proto/"],
         customize: protoc_rust_no_elision::Customize {
