cd1bb65609865324fa2d16dac1f7e7ea7718980b
elniallo <niall@hycon.io>
2019-01-30 16:19:46 +0900

    Added Hycon-core proto definitions

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 extern crate bytes;
+extern crate futures;
 extern crate tokio_core;
 extern crate tokio_io;
 extern crate tokio_proto;
-extern crate futures;
 
-pub mod protocol;
 pub mod serialization;
 pub mod server;
