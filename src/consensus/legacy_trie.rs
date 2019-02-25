aeb7244cb59b61ef88cec824f97bc5395110e7ee
elniallo <niall@hycon.io>
2019-02-25 17:12:34 +0900

    Get logic done, beginning to spec out tests for verification

--- src/consensus/legacy_trie.rs
+++ src/consensus/legacy_trie.rs
@@ -8,6 +8,7 @@ use crate::serialization::state::Account as ProtoAccount;
 use crate::traits::{Exception, Proto};
 
 use starling::traits::Database;
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::error::Error;
 // fn set_db_options() -> Options {
@@ -74,8 +75,28 @@ impl LegacyTrie {
             } else if let Some(node) = &db_state.node {
                 // we have a node
                 //insert node into seen map
-                map.entry(address[0..offset + 1].to_vec())
-                    .or_insert(db_state.clone());
+                let entry = map.entry(address[0..offset + 1].to_vec());
+                match entry {
+                    Entry::Vacant(_entry) => {
+                        map.insert(address[0..offset + 1].to_vec(), db_state.clone());
+                    }
+                    Entry::Occupied(entry) => {
+                        // get from map
+                        let next_state = entry.get().clone();
+                        // update offset
+                        if let Some(node) = &next_state.node {
+                            if let Some(node_ref) = node.node_refs.get(&vec![address[offset]]) {
+                                offset += node_ref.node_location.len();
+                                state = Some(next_state.clone());
+                                continue;
+                            }
+                        } else {
+                            return Err(Box::new(Exception::new(
+                                "Unable to find node, corrupted tree",
+                            )));
+                        }
+                    }
+                }
                 // find next node based on index and assign to state variable
                 if let Some(node_ref) = node.node_refs.get(&vec![address[offset]]) {
                     if let Some(next_node) = self.db.get_node(&node_ref.child)? {
@@ -100,5 +121,29 @@ impl LegacyTrie {
             }
         }
         Ok(None)
+    }
+}
+
+pub mod tests {
+    use super::*;
+    #[test]
+    fn it_inserts_256_keys_with_different_first_bytes_into_empty_tree_and_retrieves_them() {
+        unimplemented!();
+    }
+    #[test]
+    fn it_inserts_and_retrieves_a_key_from_an_existing_tree() {
+        unimplemented!();
+    }
+    #[test]
+    fn it_inserts_and_retrieves_two_keys_with_similar_paths() {
+        unimplemented!();
+    }
+    #[test]
+    fn it_inserts_and_retrieves_multiple_keys_from_existing_tree() {
+        unimplemented!();
+    }
+    #[test]
+    fn it_matches_typescript_world_state_for_exodus_block() {
+        unimplemented!();
     }
 }
