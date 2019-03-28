48dba14156a147ec060df361255c91b27b201b38
Niall Moore <niall@hycon.io>
2019-03-29 07:59:40 +0900

    Added fork choice trait

--- src/consensus/mod.rs
+++ src/consensus/mod.rs
@@ -2,5 +2,10 @@ pub mod consensus;
 pub mod difficulty_adjuster;
 pub mod legacy_trie;
 pub mod state_processor;
-pub mod worldstate;
 pub mod tree_node;
+pub mod worldstate;
+
+use std::cmp::Ordering;
+pub trait BlockForkChoice {
+    fn fork_choice(&self, other: &Self) -> Ordering;
+}
