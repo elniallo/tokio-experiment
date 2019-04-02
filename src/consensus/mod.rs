0a5de2755d51836821bcf4ba9f771fcb2af91293
Niall Moore <niall@hycon.io>
2019-04-02 15:34:03 +0900

    Documentation for BlockForkChoice

--- src/consensus/mod.rs
+++ src/consensus/mod.rs
@@ -6,6 +6,20 @@ pub mod tree_node;
 pub mod worldstate;
 
 use std::cmp::Ordering;
+/// # BlockForkChoice Trait
+/// Acts as a comparator between two blocks to give precedence to one over the other in the event of a forking event
+/// ___
+/// ## Required Methods
+/// ### Fork Choice
+/// ```
+/// fn fork_choice(&self, other: &Self) -> std::cmp::Ordering
+/// ```
+/// #### Arguments
+/// - `other` - some other block to compare to
+///
+/// #### Returns
+///  An ordering between self and other
+/// ___
 pub trait BlockForkChoice {
     fn fork_choice(&self, other: &Self) -> Ordering;
 }
