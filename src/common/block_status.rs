f5bdb55f695c85d28d8483219b32b2606e47dbad
Niall Moore <niall@hycon.io>
2019-04-03 11:51:04 +0900

    Generalised EnumConverter Trait

--- src/common/block_status.rs
+++ src/common/block_status.rs
@@ -1,23 +1,33 @@
 use crate::traits::Exception;
 use std::error::Error;
-
+/// Enum for the status of a Block
 #[derive(Debug, PartialEq, Clone, PartialOrd, Ord, Eq)]
 pub enum BlockStatus {
+    /// Block has Been Rejected
     Rejected,
+    /// No information Exists for Block
     Nothing,
+    /// Header has been processed
     Header,
+    /// Failed validation for some reason, Header is valid
     Invalid,
+    /// Block received but not added to longest chain
     Block,
+    /// Block is part of the currently defined heaviest chain
     MainChain,
 }
-
-pub trait EnumConverter {
-    fn to_u8(&self) -> u8;
-    fn from_u8(number: u8) -> Result<BlockStatus, Box<Error>>;
+/// Performs a conversion on an enum
+pub trait EnumConverter<OutputType> {
+    /// Returns a representation of the Enum in the form of the OutputType
+    fn to_output(&self) -> OutputType;
+    /// Returns an Enum from an input of the Specified Type
+    fn from_input(number: OutputType) -> Result<Self, Box<Error>>
+    where
+        Self: Sized;
 }
 
-impl EnumConverter for BlockStatus {
-    fn to_u8(&self) -> u8 {
+impl EnumConverter<u8> for BlockStatus {
+    fn to_output(&self) -> u8 {
         match self {
             BlockStatus::Rejected => return 0,
             BlockStatus::Nothing => return 1,
@@ -28,7 +38,7 @@ impl EnumConverter for BlockStatus {
         }
     }
 
-    fn from_u8(number: u8) -> Result<BlockStatus, Box<Error>> {
+    fn from_input(number: u8) -> Result<BlockStatus, Box<Error>> {
         match number {
             0 => return Ok(BlockStatus::Rejected),
             1 => return Ok(BlockStatus::Nothing),
@@ -52,19 +62,19 @@ mod tests {
     #[test]
     fn it_gives_u8_value_from_enum() {
         let status = BlockStatus::Rejected;
-        assert_eq!(status.to_u8(), 0)
+        assert_eq!(status.to_output(), 0)
     }
 
     #[test]
     fn it_gives_5_as_u8_from_block_status_main_chain() {
         let status = BlockStatus::MainChain;
-        assert_eq!(status.to_u8(), 5)
+        assert_eq!(status.to_output(), 5)
     }
 
     #[test]
     fn it_gives_err_from_wrong_u8() {
         assert!(
-            BlockStatus::from_u8(10).is_err(),
+            BlockStatus::from_input(10).is_err(),
             "10 is not a value for BlockStatus"
         )
     }
@@ -72,7 +82,7 @@ mod tests {
     #[test]
     fn it_gives_err_from_wrong_value() {
         assert!(
-            BlockStatus::from_u8(6).is_err(),
+            BlockStatus::from_input(6).is_err(),
             "6 is not the value for BlockStatus"
         );
     }
