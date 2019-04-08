470c81b9ab7c0c3d4f4d1a1855f390266d5cb520
Niall Moore <niall@hycon.io>
2019-04-08 14:30:43 +0900

    adding docs

--- src/util/hash.rs
+++ src/util/hash.rs
@@ -1,13 +1,13 @@
 use blake2_rfc::blake2b::blake2b;
 use cryptonight::cryptonight;
-
+/// Calculates the Blake2b Hash of the input data
 pub fn hash(data: &[u8], size: usize) -> Vec<u8> {
         let hash_data = blake2b(size, &[], data);
         let mut hash_vec = vec![0; size];
         hash_vec.clone_from_slice(&hash_data.as_bytes()[..]);
         hash_vec
 }
-
+/// Calculates the Cryptonight hash of the input
 pub fn hash_cryptonight(data: &[u8], size: usize) -> Vec<u8> {
-    cryptonight(data, size, 1 /* Monero v7 */)
+        cryptonight(data, size, 1 /* Monero v7 */)
 }
